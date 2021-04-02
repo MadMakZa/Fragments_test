a786833eb32924260f57ba8eeecbca50d32a30aa
MadMakZa <immakza@gmail.com>
2021-04-02 22:25:40 +0300

    just test fragments 111

--- app/src/main/java/com/examples/fragments_test/MainActivity.kt
+++ app/src/main/java/com/examples/fragments_test/MainActivity.kt
@@ -2,10 +2,27 @@ package com.examples.fragments_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var btnOne: Button = findViewById(R.id.btnFragment1)
+    var btnTwo: Button = findViewById(R.id.btnFragment1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+    
+    private fun buttonsClick(){
+        btnOne.setOnClickListener {
+            //запуск первого фрагмента
+
+        }
+    }
+    
 }
