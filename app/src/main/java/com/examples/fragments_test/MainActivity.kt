146e3609f8e061b8ad7881565871de93653e225f
MadMakZa <immakza@gmail.com>
2021-04-02 22:49:20 +0300

    just test fragments work

--- app/src/main/java/com/examples/fragments_test/MainActivity.kt
+++ app/src/main/java/com/examples/fragments_test/MainActivity.kt
@@ -7,21 +7,45 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    var btnOne: Button = findViewById(R.id.btnFragment1)
-    var btnTwo: Button = findViewById(R.id.btnFragment1)
+    private lateinit var btnOne: Button
+    private lateinit var btnTwo: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnOne = findViewById(R.id.btnFragment1)
+        btnTwo = findViewById(R.id.btnFragment2)
+
+        setFragments()
+
+
 
 
     }
     
-    private fun buttonsClick(){
+    private fun setFragments(){
+        //присвоить переменным экземпляры классов фрагментов
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        //установить по умолчанию фрагмент
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, secondFragment)
+            commit()
+        }
+        //запуск первого фрагмента
         btnOne.setOnClickListener {
-            //запуск первого фрагмента
-
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                commit()
+            }
+        }
+        //запуск второго фрагмента
+        btnTwo.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                commit()
+            }
         }
     }
     
