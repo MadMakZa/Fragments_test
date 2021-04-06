94d83e2a2f69a4c0a449ad003c215e6fce670d94
MadMakZa <immakza@gmail.com>
2021-04-06 20:13:28 +0300

    test change colors

--- app/src/main/java/com/examples/fragments_test/MainActivity.kt
+++ app/src/main/java/com/examples/fragments_test/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnOne: Button
     private lateinit var btnTwo: Button
     private lateinit var btnSA: Button
+    private lateinit var btnColor: Button
+
+    private lateinit var image1: ImageView
+    private lateinit var image2: ImageView
+    private lateinit var image3: ImageView
+    private lateinit var image4: ImageView
+    private lateinit var image5: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +28,41 @@ class MainActivity : AppCompatActivity() {
         btnOne = findViewById(R.id.btnFragment1)
         btnTwo = findViewById(R.id.btnFragment2)
         btnSA = findViewById(R.id.btnStartActivity)
+        btnColor = findViewById(R.id.btnChangeColor)
+        image1 = findViewById(R.id.id_rectangle)
+        image2 = findViewById(R.id.imageView1)
+        image3 = findViewById(R.id.imageView2)
+        image4 = findViewById(R.id.imageView3)
+        image5 = findViewById(R.id.imageView4)
 
         setFragments()
+        changeColor()
 
 
 
 
+    }
+    //сменить цвет вьюх во фрагментах
+    private fun changeColor(){
+        var flag = false
+        btnColor.setOnClickListener {
+            if(flag) {
+                image1.setImageResource(R.color.red)
+                image2.setImageResource(R.color.red)
+                image3.setImageResource(R.color.red)
+                image4.setImageResource(R.color.red)
+                image5.setImageResource(R.color.red)
+                flag = true
+            }
+            if(!flag) {
+                image1.setImageResource(R.color.orange)
+                image2.setImageResource(R.color.orange)
+                image3.setImageResource(R.color.orange)
+                image4.setImageResource(R.color.orange)
+                image5.setImageResource(R.color.orange)
+                flag = false
+            }
+        }
     }
     
     private fun setFragments(){
@@ -66,6 +103,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent, options.toBundle())
             finish()
         }
+
     }
     
 }
